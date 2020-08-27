1594bc680d7538b06f74d4d07c51a75f23c3dd6b
Sanitarium <nikit45311@gmail.com>
2020-08-27 21:21:57 +0300

    Script with generator zombies

---
+++ Assets/Scripts/SpawnZombie.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnZombie : MonoBehaviour
+{
+    private float _time;
+    [SerializeField] private GameObject _zombie;
+    [SerializeField] private Transform _spawn;
+
+    private Transform[] _spawns;
+    private int _count = 0;
+
+    private void Start()
+    {
+        Transform[] spawns = new Transform[_spawn.childCount];
+
+        for (int i = 0; i < _spawn.childCount; i++)
+        {
+            spawns[i] = _spawn.GetChild(i);
+        }
+        _spawns = spawns;
+    }
+
+    private void Update()
+    {
+        _time += Time.deltaTime;
+
+        if (_time >= 2)
+        {
+            GameObject gameObject = Instantiate(_zombie, _spawns[_count].position, Quaternion.identity);
+            gameObject.SetActive(true);
+
+            Debug.Log(_time);
+            _time = 0;
+
+            _count++;
+
+            if (_count == _spawns.Length)
+                _count = 0;
+        }
+    }
+}
